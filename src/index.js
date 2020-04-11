14dd58dcdbe6c0c7918f7efeb1054989d3c1bed5
kko0831 <kko0831@hanmail.net>
2020-04-11 17:32:24 +0900

    GraphQL - Node Tutorial - 7. Authentication

--- src/index.js
+++ src/index.js
@@ -1,37 +1,24 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
-
-async function main() {
-  const newLink = await prisma.createLink({
-    url: "www.prisma.io",
-    description: "Prisma replaces traditional ORMs"
-  });
-  console.log(`Created new link: ${newLink.url} (ID: ${newLink.id})`);
-  const allLinks = await prisma.links();
-  console.log(allLinks);
-}
-main().catch(e => console.error(e));
-
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone.`,
-    feed: (root, args, context, info) => {
-      return context.prisma.links();
-    }
-  },
-  Mutation: {
-    post: (root, args, context) => {
-      return context.prisma.createLink({
-        url: args.url,
-        description: args.description
-      });
-    }
-  }
+  Query,
+  Mutation,
+  User,
+  Link
 };
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers,
-  context: { prisma }
+  context: request => {
+    return {
+      ...request,
+      prisma
+    };
+  }
 });
 server.start(() => console.log(`http://localhost:4000에서 서버 가동 중`));
